32e1ee1381c1f9348fde5ea9ac5fbf590dcd7d02
Berker OĞUZ <tberkeroguz@gmail.com>
2022-10-17 14:45:41 +0300

    day17

---
+++ batch105Java/src/ders13_nastedForLoop_methodOlusturma/C09_MethodKullanma.java
@@ -0,0 +1,7 @@
+package ders13_nastedForLoop_methodOlusturma;
+
+public class C09_MethodKullanma {
+    public static void main(String[] args) {
+        C08_MethodOlusturma.isteToplaYazdir();
+    }
+}
